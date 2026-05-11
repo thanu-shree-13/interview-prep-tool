622fe2ec3cf979c3a1e06ee06c97a1a1dfaf6864
thanu-shree-13 <thanushreet42@gmail.com>
2026-05-11 21:05:54 +0530

    Serve React frontend from Express

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,10 @@ app.use('/api/questions', require('./routes/questions'))
 app.use('/api/sessions', require('./routes/sessions'))
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 5000
